ffc681e91c9a67b6d779d92433d1503aeba6c15e
Abeer <abeer.technology@gmail.com>
2020-11-19 13:46:09 +0600

    Added connect method in index.js

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const genres = require('./routes/genres');
 const express = require('express');
 const app = express();
 
+mongoose.connect('mongodb://localhost/vidly-cleaner', { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(()=> console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Could not connect to MongoDB...'));
+
 app.use(express.json());
 app.use('/api/genres', genres);
 
